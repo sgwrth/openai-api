b882d2c368478c39617dd49aa3079b66e66ab40d
Andreas Siegwarth <andreassiegwarth@startmail.com>
2024-05-13 15:54:12 +0200

    added OpenAPI dependency to POM added @CrossOrigin annotation to endpoint

--- src/main/java/org/springframework/ai/openai/samples/helloworld/simple/SimpleAiController.java
+++ src/main/java/org/springframework/ai/openai/samples/helloworld/simple/SimpleAiController.java
@@ -2,6 +2,7 @@ package org.springframework.ai.openai.samples.helloworld.simple;
 
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class SimpleAiController {
 		this.chatClient = chatClient;
 	}
 
+  @CrossOrigin
 	@GetMapping("/ai/simple")
 	public Map<String, String> completion(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
 		return Map.of("generation", chatClient.call(message));
